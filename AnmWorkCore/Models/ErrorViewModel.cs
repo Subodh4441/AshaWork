ab66ed00ab30c63785bbbe96124cb6500cbdd3f4
Rohit <rohit.madane@celusion.com>
2025-01-24 23:16:38 +0530

    First push
    First push

--- AnmWorkCore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AnmWorkCore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
